7c1541f60cc8cee5f37f378e0550f72e522b686f
kaushiksri264 <kaushiksri264@gmail.com>
2025-09-09 09:20:49 +0000

    Added Dynamic Elements

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+jobs = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Irving, Tx',
+    'salary': '$ 150000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Austin, Tx',
+    'salary': '$ 120000'
+}, {
+    'id': 3,
+    'title': 'Data Engineer',
+    'location': 'Irvine, CA'
+}, {
+    'id': 4,
+    'title': 'Data Administrater',
+    'location': 'New York, NYC',
+    'salary': '$ 220000'
+}]
 
 
 @app.route("/")
 def hello_lookup():
-  return render_template('home.html')
+    return render_template(
+        'home.html', jobs=jobs,
+        company_name='LookUp')  #passes the jobs list to the html file
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
